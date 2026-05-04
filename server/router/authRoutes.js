859e272a71783bbcb98e685c036348e86c9420be
mahi <mahtab.mahi707@gamail.com>
2026-05-04 18:24:29 +0600

    feat/ created auth-middleware and login.logout,getuser func

--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { getUserProfile, login, logout, register } from '../controllers/authController.js';
+import { isAuthenticatedUser } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', register)
 router.post('/login', login)
-router.get('/me', getUserProfile)
-router.get('/logout', logout)
+router.get('/me',isAuthenticatedUser, getUserProfile)
+router.get('/logout',isAuthenticatedUser, logout)
 export default router;
